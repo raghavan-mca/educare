b81a6e40798d709ea1623bc2aaff6d4db022306e
TeckgeekARJ <57619911+TeckgeekARJ@users.noreply.github.com>
2022-04-15 13:54:39 +0530

    Deleted Non commited frontend
    Deleted Non commited frontend

--- placement-management/src/app/filter-search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterSearchPipe } from './filter-search.pipe';
-
-describe('FilterSearchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterSearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
